e94bfedb14b3fe98c76834e4034a5d7416e80f13
Shahali Bogdadi <bogdadishahali@gmail.com>
2020-03-03 13:38:21 +0600

    updated

--- works.js
+++ works.js
@@ -1,21 +1,26 @@
 [{
-    name: 'Jagonews24.com',
-    type: 'News Portal',
-    url: 'https://jagonews24.com/'
-}, {
-    name: 'Jagojobs',
-    type: 'Job Portal',
-    url: 'https://www.jagojobs.com/'
-}, {
-    name: 'Jago FM 94.4',
-    type: 'Online FM Radio Website',
-    url: 'http://jago.fm/'
-}, {
-    name: 'Jagobangla.com',
-    type: 'News Portal',
-    url: 'https://www.jagobangla.com/'
-}, {
-    name: 'Banglanews24.com',
-    type: 'News Portal',
-    url: 'https://www.banglanews24.com/'
-}]
+        "name": "Jagonews24.com",
+        "type": "News Portal",
+        "url": "https://jagonews24.com/"
+    },
+    {
+        "name": "Jagojobs",
+        "type": "Job Portal",
+        "url": "https://www.jagojobs.com/"
+    },
+    {
+        "name": "Jago FM 94.4",
+        "type": "Online FM Radio Website",
+        "url": "http://jago.fm/"
+    },
+    {
+        "name": "Jagobangla.com",
+        "type": "News Portal",
+        "url": "https://www.jagobangla.com/"
+    },
+    {
+        "name": "Banglanews24.com",
+        "type": "News Portal",
+        "url": "https://www.banglanews24.com/"
+    }
+]
