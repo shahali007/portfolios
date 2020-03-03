14e3149d9d8e471e165952a93ecf78acf91599ef
Shahali bogdadi <bogdadishahali@gmail.com>
2020-03-03 23:26:59 +0600

    updated portfolio items.

--- works.js
+++ works.js
@@ -1,35 +1,147 @@
-[{
-        "name": "Jagonews24.com",
-        "type": "News Portal",
-        "url": "https://jagonews24.com/"
-    },
-    {
-        "name": "Jagojobs",
-        "type": "Job Portal",
-        "url": "https://www.jagojobs.com/"
-    },
-    {
-        "name": "Jago FM 94.4",
-        "type": "Online FM Radio Website",
-        "url": "http://jago.fm/"
-    },
-    {
-        "name": "Jagobangla.com",
-        "type": "News Portal",
-        "url": "https://www.jagobangla.com/"
-    },
-    {
-        "name": "Banglanews24.com",
-        "type": "News Portal",
-        "url": "https://www.banglanews24.com/"
-    },
-    {
-        "name": "Maxxcola",
-        "type": "Company Website",
-        "url": "http://maxxcola.com/"
-    }, {
-        "name": "Property Development Ltd",
-        "type": "Company Website",
-        "url": "http://www.propertydevelopmentltd.com/"
-    }
+[
+   {
+      "name":"Jagonews24.com",
+      "type":"News Portal",
+      "url":"https://jagonews24.com/"
+   },
+   {
+      "name":"Jagojobs",
+      "type":"Job Portal",
+      "url":"https://www.jagojobs.com/"
+   },
+   {
+      "name":"Jago FM 94.4",
+      "type":"Online FM Radio Website",
+      "url":"http://jago.fm/"
+   },
+   {
+      "name":"Surecell BD",
+      "type":"Medical Website",
+      "url":"https://surecellbd.com/"
+   },
+   {
+      "name":"Hashtag",
+      "type":"ISP website",
+      "url":"http://hashtag.com.bd/"
+   },
+   {
+      "name":"Digital Bangladesh Mela 2020",
+      "type":"Conference website",
+      "url":"https://www.digitalbangladeshmela.org.bd/"
+   },
+   {
+      "name":"71news",
+      "type":"News portal",
+      "url":"http://71news.net/"
+   },
+   {
+      "name":"The BD Press",
+      "type":"News portal",
+      "url":"http://thebdpress.com/"
+   },
+   {
+      "name":"Red Sparrow Digital",
+      "type":"Digital Marketing Website",
+      "url":"https://redsparrowdigital.com/draft"
+   },
+   {
+      "name":"Broadband247",
+      "type":"ISP Website",
+      "url":"http://broadband247.net/"
+   },
+   {
+      "name":"Doshdiganta.com",
+      "type":"News Portal",
+      "url":"https://www.doshdiganta.com/"
+   },
+   {
+      "name":"Chhabiwala",
+      "type":"Personal Website",
+      "url":"https://chhabiwala.com/"
+   },
+   {
+      "name":"Samhati",
+      "type":"Books library",
+      "url":"http://samhati.com/"
+   },
+   {
+      "name":"Data Insights Technologies",
+      "type":"Company Website",
+      "url":"https://dittechs.com/"
+   },
+   {
+      "name":"Twenty Four April",
+      "type":"Remembrance of Rana Plaza Victims",
+      "url":"http://www.twentyfourapril.com/"
+   },
+   {
+      "name":"Avenue",
+      "type":"Hotel & Motel",
+      "url":"http://cloudlylab.com/projects/avenue/"
+   },
+   {
+      "name":"Maxxcola",
+      "type":"Company Website",
+      "url":"http://maxxcola.com/"
+   },
+   {
+      "name":"Property Development Ltd",
+      "type":"Company Website",
+      "url":"http://www.propertydevelopmentltd.com/"
+   },
+   {
+      "name":"Home Health Care Point",
+      "type":"Health Care & Tips website",
+      "url":"http://homehealthcarepoint.com/"
+   },
+   {
+      "name":"Dhaka Prakash",
+      "type":"News Portal",
+      "url":"http://dhakaprokash24.com/"
+   },
+   {
+      "name":"Provat Feri",
+      "type":"News Portal in Australia",
+      "url":"http://provatferi.com.au/"
+   },
+   {
+      "name":"Multicultural Society",
+      "type":"Australian community based website",
+      "url":"http://multiculturalsociety.org/"
+   },
+   {
+      "name":"Mutual Homes",
+      "type":"Land & Property website in Australia",
+      "url":"http://mutualhomes.com.au/"
+   },
+   {
+      "name":"Newsnarayangang24.net",
+      "type":"News Portal",
+      "url":"https://www.newsnarayanganj24.net/"
+   },
+   {
+      "name":"Jagobangla.com",
+      "type":"News Portal",
+      "url":"https://www.jagobangla.com/"
+   },
+   {
+      "name":"Sportsmail24.com",
+      "type":"News Portal",
+      "url":"http://sportsmail24.com/"
+   },
+   {
+      "name":"Narsingditimes.com",
+      "type":"News Portal",
+      "url":"https://narsingditimes.com/"
+   },
+   {
+      "name":"Banglanews24.com",
+      "type":"News Portal",
+      "url":"https://www.banglanews24.com/"
+   },
+   {
+      "name":"Shazulinews.com",
+      "type":"News Portal",
+      "url":"http://www.shazulinews.com/"
+   }
 ]
